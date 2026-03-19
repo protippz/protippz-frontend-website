b6b3b2daf47b23f94641b5b8aadf6e1439ab653b
hosainahmeed <skyfal430@gmail.com>
2026-03-19 11:33:08 +0600

    update

--- next.config.ts
+++ next.config.ts
@@ -19,35 +19,36 @@ const nextConfig: NextConfig = {
     ];
   },
   images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: '10.10.20.9',
-        port: '5000',
-      },
-      {
-        protocol: 'http',
-        hostname: '10.0.60.137',
-        port: '5050',
-      },
-      {
-        protocol: 'http',
-        hostname: '10.0.60.37',
-        port: '5050',
-      },
-      {
-        protocol: 'https',
-        hostname: 'api.protippz.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'protippz-bucket.s3.us-east-2.amazonaws.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'dsuotz3idqy4q.cloudfront.net',
-      },
-    ],
+    // remotePatterns: [
+    //   {
+    //     protocol: 'http',
+    //     hostname: '10.10.20.9',
+    //     port: '5000',
+    //   },
+    //   {
+    //     protocol: 'http',
+    //     hostname: '10.0.60.137',
+    //     port: '5050',
+    //   },
+    //   {
+    //     protocol: 'http',
+    //     hostname: '10.0.60.37',
+    //     port: '5050',
+    //   },
+    //   {
+    //     protocol: 'https',
+    //     hostname: 'api.protippz.com',
+    //   },
+    //   {
+    //     protocol: 'https',
+    //     hostname: 'protippz-bucket.s3.us-east-2.amazonaws.com',
+    //   },
+    //   {
+    //     protocol: 'https',
+    //     hostname: 'dsuotz3idqy4q.cloudfront.net',
+    //   },
+    // ],
+    unoptimized: true
   },
 
 };
