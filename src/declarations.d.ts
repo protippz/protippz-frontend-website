16bfeae853f6d11899b3ed061b3cd64f4dd87953
hosainahmed <hosainahmed534745@gmail.com>
2026-09-04 11:38:16 +0600

    updaye

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -2,3 +2,46 @@ declare module "lodash.debounce" {
     import debounce from "lodash";
     export default debounce;
 }
+
+declare module "*.png" {
+    import { StaticImageData } from "next/image";
+    const content: StaticImageData;
+    export default content;
+}
+
+declare module "*.jpg" {
+    import { StaticImageData } from "next/image";
+    const content: StaticImageData;
+    export default content;
+}
+
+declare module "*.jpeg" {
+    import { StaticImageData } from "next/image";
+    const content: StaticImageData;
+    export default content;
+}
+
+declare module "*.svg" {
+    import { StaticImageData } from "next/image";
+    const content: StaticImageData;
+    export default content;
+}
+
+declare module "*.webp" {
+    import { StaticImageData } from "next/image";
+    const content: StaticImageData;
+    export default content;
+}
+
+declare module "*.gif" {
+    import { StaticImageData } from "next/image";
+    const content: StaticImageData;
+    export default content;
+}
+
+declare module "*.ico" {
+    import { StaticImageData } from "next/image";
+    const content: StaticImageData;
+    export default content;
+}
+
