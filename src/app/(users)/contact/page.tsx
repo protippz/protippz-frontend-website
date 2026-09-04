9d63e9d72dd11b5a5a38759703d556f28626c39e
DevByJake <saikathossain826@gmail.com>
2026-09-04 11:49:32 +0600

    Update landing page

--- src/app/(users)/contact/page.tsx
+++ src/app/(users)/contact/page.tsx
@@ -19,6 +19,7 @@ const ContactUsPage: React.FC = () => {
       setLoading(false);
     }
   };
+  // Update landing page
   return (
     <div
       className="flex items-center justify-center min-h-screen w-full"
